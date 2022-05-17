0dedeae0cd14542958ba8a5c9b5ccddb3e80af4c
Andrea Da Silva <adasilvapdev@gmail.com>
2022-05-17 17:50:39 -0400

    Initial structure :)

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Initial structure</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'black',
+  },
+});
+
+export default App;
